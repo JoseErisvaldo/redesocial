0f607834df827539d0f8cedb7f3f86bb3b1c3912
Jose Erisvaldo <joseerisvaldopg@gmail.com>
2024-06-16 15:56:02 -0300

    ✨Feat: Private

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,18 +1,36 @@
+import React, { useContext } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './Pages/Home/Home'
 import Header from './Components/Header/Header'
 import Sidebar from './Components/Sidebar/SideBar'
+import AuthProvider, { AuthContext } from './Components/Context/Login'
+import Login from './Pages/Login/Login'
 
 export default function RoutesApp() {
+  const AdminPrivate = ({ children }) => {
+    const { user } = useContext(AuthContext)
+    if (!user) {
+      return (
+        <>
+          <Login />
+        </>
+      )
+    }
+    return children
+  }
   return (
     <BrowserRouter>
-      <Sidebar />
-      <div className="flex flex-col w-full">
-        <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-        </Routes>
-      </div>
+      <AuthProvider>
+        <AdminPrivate>
+          <Sidebar />
+          <div className="flex flex-col w-full">
+            <Header />
+            <Routes>
+              <Route path="/" element={<Home />} />
+            </Routes>
+          </div>
+        </AdminPrivate>
+      </AuthProvider>
     </BrowserRouter>
   )
 }
